09cffad8cd1390117175dfb866280b234c5fddde
xiaoranran <xiaoranran1993@outlook.com>
2019-09-26 16:20:12 +0800

    change app title

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Todos',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
